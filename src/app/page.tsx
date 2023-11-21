6575ae0a8310f9623d4382a653e4ec2a11c23341
YONG JUN CHOI <67721584+codeCYJ@users.noreply.github.com>
2023-11-21 15:52:39 +0900

    feature - mainBG
    mainpage image 클릭시 게시판으로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,19 @@
 
 import Image from "next/image";
 
-import main from "@/assets/main/main.png";
 import flag from "@/assets/main/flag.png";
 import Board from "@/components/templates/main/Board";
 import { Container } from "@/components/atoms";
 import Banner from "@/components/templates/banner";
 import Nav from "@/components/organisms/Nav";
+import ImageContainer from "@/components/templates/main/ImageContainer";
 
 const Home = () => {
   return (
     <>
       <Nav />
       <Container>
-        <Image src={main} alt="" />
+        <ImageContainer />
         <div className="w-full flex justify-end">
           <Image src={flag} alt="" className="my-10" />
         </div>
