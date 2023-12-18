aae8d87de93eb3474c1bfa75ecd7e4ad7c2aef29
YONG JUN CHOI <67721584+codeCYJ@users.noreply.github.com>
2023-12-18 17:41:00 +0900

    feat - ssrBoard
    글목록 ssr 적용

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,7 +1,29 @@
 import api from "@/apis/client";
+import { getPeriod } from "@/utils/getDate";
+
 import Client from "@/components/templates/board";
 
-export default async function Board() {
+export default async function Board({ searchParams }: any) {
+  const { page, camp, category, type, keyword } = searchParams;
+  const { startDate, endDate } = getPeriod(type || "d");
+  const isCamp = !!camp;
+
+  const campParams = {
+    page: page || 1,
+    keyword: keyword || "",
+    politicalOrientationId: camp || null,
+    categoryId: category || null,
+  };
+
+  const params = {
+    page: page || 1,
+    keyword: keyword || "",
+    politicalOrientationId: camp || null,
+    categoryId: category || null,
+    startDate,
+    endDate,
+  };
+
   async function getAds() {
     try {
       const response = await api.get("ad");
@@ -11,7 +33,31 @@ export default async function Board() {
     }
   }
 
-  const ads = await getAds();
+  async function getPostings() {
+    try {
+      const response = await api.get("posting", {
+        params: isCamp ? campParams : params,
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  return <Client ads={ads} />;
+  async function getHotPostings() {
+    try {
+      const response = await api.get("posting/hot", {
+        params: isCamp ? campParams : params,
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const ads = await getAds();
+  const posts = await getPostings();
+  const hotPosts = await getHotPostings();
+
+  return <Client ads={ads} posts={posts} hotPosts={hotPosts} />;
 }
